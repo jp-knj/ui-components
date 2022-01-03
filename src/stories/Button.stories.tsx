a2042d014e2ed775b9b87f133ec3db6b3e5a46aa
mono-tmt <tomita@monosus.co.jp>
2022-01-03 14:37:53 +0900

    :sparkles: show primary button

--- src/stories/Button.stories.tsx
+++ src/stories/Button.stories.tsx
@@ -17,7 +17,7 @@ export default {
 // More on component templates: https://storybook.js.org/docs/preact/writing-stories/introduction#using-args
 const Template:Story<Props> = (args:Props) => <Button {...args} />;
 
-const Primary = Template.bind({});
+export const Primary = Template.bind({});
 
 // More on args: https://storybook.js.org/docs/preact/writing-stories/args
 Primary.args = {
