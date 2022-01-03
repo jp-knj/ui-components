36a7bdc88c933aec74a1249e085104761f7e221d
jp-knj <kenji.tomita1996@gmail.com>
2022-01-03 17:33:55 +0900

    :art: create button component

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import preact from '@preact/preset-vite'
+// import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
 
 // https://vitejs.dev/config/
 export default defineConfig({
